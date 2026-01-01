695c9f2bee3f5e57540ef7442829c81a9c3c9e87
Nguyen Trung Kien <nguyentrungkien040921@gmail.com>
2026-01-01 22:24:15 +0700

    upload code

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosConfig = axios.create({
-  // baseURL: "https://quan-ly-ban-quan-ao-server.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://quan-ly-ban-quan-ao-server.onrender.com",
+  // baseURL: "http://localhost:8080",
   timeout: 60000,
   withCredentials: true,
   headers: {
